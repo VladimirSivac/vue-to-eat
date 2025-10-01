feb475dffc54df5dbcd931a42abf21e290abd475
VladimirSivac <morndes@gmail.com>
2025-10-01 22:08:53 +0300

    update

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import TheNavbar from './components/TheNavbar.vue'
+
+export default {
+  components: {
+    TheNavbar,
+  },
+  data: () => ({
+    tagline: 'Test String',
+  }),
+}
+</script>
+
+<template>
+  <TheNavbar :tagline="tagline" />
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const restaurantStatusList = [
+  'Want to Try',
+  'Recommended',
+  'Do Not Recommend',
+  'Delicious',
+] as const
+
+export const dietList = ['Vegetarian', 'Pescetarian', 'Keto', ''] as const
